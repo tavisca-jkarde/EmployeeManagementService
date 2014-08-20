f2160cd39398d36e112a1ccc7866bbabbc8cca51
jkarde <jkarde@tavisca.com>
2014-08-20 10:05:53 +0530

    XML data Source added

--- EmployeeManagementServiceFixture/TestEmployeeService.cs
+++ EmployeeManagementServiceFixture/TestEmployeeService.cs
@@ -9,12 +9,12 @@ namespace EmployeeManagementServiceFixture
     [TestClass]
     public class TestEmployeeService
     {
-        private TestContext testContextInstance;
+        private TestContext _testContextInstance;
 
         public TestContext TestContext
         {
-            get { return testContextInstance; }
-            set { testContextInstance = value; }
+            get { return _testContextInstance; }
+            set { _testContextInstance = value; }
         }
 
         /// <summary>
@@ -34,20 +34,20 @@ namespace EmployeeManagementServiceFixture
         /// then retrive employee to check the existing employee same or not.
         /// </summary>
         [TestMethod]
-        [DeploymentItem(@"E:\WCFAssignment\EmployeeManagementService\EmployeeManagementServiceFixture\EmployeeDataSource.xml")]
+        [DeploymentItem(@"EmployeeDataSource.xml")]
         [DataSource("Microsoft.VisualStudio.TestTools.DataSource.XML",
-                   @"E:\WCFAssignment\EmployeeManagementService\EmployeeManagementServiceFixture\EmployeeDataSource.xml",
+                   @"EmployeeDataSource.xml",
                    "Employee",
                     DataAccessMethod.Sequential)]
         public void CreateEmployeeShouldThrow()
         {
             using (var employeeList = new AddEmployeeServiceClient("BasicHttpBinding_AddEmployeeService"))
             {
-                DateTime date = DateTime.Now;
+                DateTime date = DateTime.Today;
 
-                var employeeId = Convert.ToInt32(testContextInstance.DataRow["EmployeeId"].ToString());
-                var employeeName = testContextInstance.DataRow["EmployeeName"].ToString();
-                var employeeRemark = testContextInstance.DataRow["EmployeeRemark"].ToString();
+                var employeeId = Convert.ToInt32(_testContextInstance.DataRow["EmployeeId"].ToString());
+                var employeeName = _testContextInstance.DataRow["EmployeeName"].ToString();
+                var employeeRemark = _testContextInstance.DataRow["EmployeeRemark"].ToString();
 
                 employeeList.CreateEmployee(employeeId, employeeName, employeeRemark, date);
                 var employeeRetrive = new RetriveEmployeeServiceClient("WSHttpBinding_RetriveEmployeeService");
@@ -72,18 +72,28 @@ namespace EmployeeManagementServiceFixture
         /// Then check the the remark of that employee.
         /// </summary>
         [TestMethod]
-
+        [DeploymentItem(@"EmployeeDataSource.xml")]
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.XML",
+                   @"EmployeeDataSource.xml",
+                   "Employee",
+                    DataAccessMethod.Sequential)]
         public void AddRemarkShouldThrow()
         {
 
             using (var employeeList = new AddEmployeeServiceClient("BasicHttpBinding_AddEmployeeService"))
             {
-                employeeList.CreateEmployee(101, "Jayvant", "Bad", DateTime.Today);
+                DateTime date = DateTime.Today;
 
-                employeeList.AddRemark("Good", 101);
+                var employeeId = Convert.ToInt32(_testContextInstance.DataRow["EmployeeId"].ToString());
+                var employeeName = _testContextInstance.DataRow["EmployeeName"].ToString();
+                var employeeRemark = _testContextInstance.DataRow["EmployeeRemark"].ToString();
+
+                employeeList.CreateEmployee(employeeId, employeeName, employeeRemark, date);
+
+                employeeList.AddRemark("Good", employeeId);
 
                 var employeeRetrive = new RetriveEmployeeServiceClient("WSHttpBinding_RetriveEmployeeService");
-                var retriveEmployee = employeeRetrive.SearchById(101);
+                var retriveEmployee = employeeRetrive.SearchById(employeeId);
 
                 if (retriveEmployee != null)
                 {
@@ -101,14 +111,26 @@ namespace EmployeeManagementServiceFixture
         /// If employee Id does not exist in database, it will fail.
         /// </summary>
         [TestMethod]
+        
+        [DeploymentItem(@"EmployeeDataSource.xml")]
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.XML",
+                   @"EmployeeDataSource.xml",
+                   "Employee",
+                    DataAccessMethod.Sequential)]
         public void GetEmployeeDetailsByIdShouldThrow()
         {
             using (var employeeList = new AddEmployeeServiceClient("BasicHttpBinding_AddEmployeeService"))
             {
                 using (var employeeRetrive = new RetriveEmployeeServiceClient("WSHttpBinding_RetriveEmployeeService"))
                 {
-                    employeeList.CreateEmployee(101, "Jayvant", "Bad", DateTime.Today);
-                    var retriveEmployee = employeeRetrive.SearchById(101);
+                    DateTime date = DateTime.Today;
+
+                    var employeeId = Convert.ToInt32(_testContextInstance.DataRow["EmployeeId"].ToString());
+                    var employeeName = _testContextInstance.DataRow["EmployeeName"].ToString();
+                    var employeeRemark = _testContextInstance.DataRow["EmployeeRemark"].ToString();
+
+                    employeeList.CreateEmployee(employeeId, employeeName, employeeRemark, date);
+                    var retriveEmployee = employeeRetrive.SearchById(employeeId);
                     if (retriveEmployee == null)
                     {
                         Assert.Fail("Employee Does not exist");
@@ -123,16 +145,26 @@ namespace EmployeeManagementServiceFixture
         /// If employee Name does not exist in database, it will fail.
         /// </summary>
         [TestMethod]
-
+        [DeploymentItem(@"EmployeeDataSource.xml")]
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.XML",
+                   @"EmployeeDataSource.xml",
+                   "Employee",
+                    DataAccessMethod.Sequential)]
         public void GetEmployeeDetailsByNameShouldThrow()
         {
             using (var employeeList = new AddEmployeeServiceClient("BasicHttpBinding_AddEmployeeService"))
             {
                 using (var employeeRetrive = new RetriveEmployeeServiceClient("WSHttpBinding_RetriveEmployeeService"))
                 {
-                    employeeList.CreateEmployee(101, "Jayvant", "Bad", DateTime.Today);
-                    string employeeName = "Jayvant";
-                    var retriveEmployee = employeeRetrive.SearchByName(employeeName);
+                    DateTime date = DateTime.Today;
+
+                    var employeeId = Convert.ToInt32(_testContextInstance.DataRow["EmployeeId"].ToString());
+                    var employeeName = _testContextInstance.DataRow["EmployeeName"].ToString();
+                    var employeeRemark = _testContextInstance.DataRow["EmployeeRemark"].ToString();
+
+                    employeeList.CreateEmployee(employeeId, employeeName, employeeRemark, date);
+                    string employeeNameValue = "Jayvant";
+                    var retriveEmployee = employeeRetrive.SearchByName(employeeNameValue);
                     if (retriveEmployee == null)
                     {
                         Assert.Fail("Employee Does not exist");
@@ -149,6 +181,11 @@ namespace EmployeeManagementServiceFixture
         /// If employee Remark does not exist in database, it will fail.
         /// </summary>
         [TestMethod]
+        [DeploymentItem(@"EmployeeDataSource.xml")]
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.XML",
+                   @"EmployeeDataSource.xml",
+                   "Employee",
+                    DataAccessMethod.Sequential)]
         public void GetEmployeeDetailsByRemarkShouldThrow()
         {
             try
@@ -157,7 +194,12 @@ namespace EmployeeManagementServiceFixture
                 {
                     using (var employeeRetrive = new RetriveEmployeeServiceClient("WSHttpBinding_RetriveEmployeeService"))
                     {
-                        employeeList.CreateEmployee(101, "Jayvant", "Bad", DateTime.Today);
+                        DateTime date = DateTime.Today;
+                        var employeeId = Convert.ToInt32(_testContextInstance.DataRow["EmployeeId"].ToString());
+                        var employeeName = _testContextInstance.DataRow["EmployeeName"].ToString();
+                        var employeeRemark = _testContextInstance.DataRow["EmployeeRemark"].ToString();
+
+                        employeeList.CreateEmployee(employeeId, employeeName, employeeRemark, date);
                         string remark = "Good";
                         var retriveEmployee = employeeRetrive.SearchByRemark(remark);
                         if (retriveEmployee == null)
@@ -180,13 +222,23 @@ namespace EmployeeManagementServiceFixture
         /// Check the length of employee
         /// </summary>
         [TestMethod]
+        [DeploymentItem(@"EmployeeDataSource.xml")]
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.XML",
+                   @"EmployeeDataSource.xml",
+                   "Employee",
+                    DataAccessMethod.Sequential)]
         public void GetAllEmployeeShouldThrow()
         {
             using (var employeeList = new AddEmployeeServiceClient("BasicHttpBinding_AddEmployeeService"))
             {
                 using (var employeeRetrive = new RetriveEmployeeServiceClient("WSHttpBinding_RetriveEmployeeService"))
                 {
-                    employeeList.CreateEmployee(101, "Jayvant", "Bad", DateTime.Today);
+                    DateTime date = DateTime.Today;
+                    var employeeId = Convert.ToInt32(_testContextInstance.DataRow["EmployeeId"].ToString());
+                    var employeeName = _testContextInstance.DataRow["EmployeeName"].ToString();
+                    var employeeRemark = _testContextInstance.DataRow["EmployeeRemark"].ToString();
+
+                    employeeList.CreateEmployee(employeeId, employeeName, employeeRemark, date);
                     var retriveEmployee = employeeRetrive.GetAllEmployeeDetails();
                     Assert.AreEqual(retriveEmployee.Length, 1);
                 }
@@ -197,6 +249,11 @@ namespace EmployeeManagementServiceFixture
         /// Below test case to check the employee remark.
         /// </summary>
         [TestMethod]
+        [DeploymentItem(@"EmployeeDataSource.xml")]
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.XML",
+                   @"EmployeeDataSource.xml",
+                   "Employee",
+                    DataAccessMethod.Sequential)]
         public void CheckEmployeeRemarkShouldThrow()
         {
             try
@@ -205,7 +262,12 @@ namespace EmployeeManagementServiceFixture
                 {
                     using (var employeeRetrive = new RetriveEmployeeServiceClient("WSHttpBinding_RetriveEmployeeService"))
                     {
-                        employeeList.CreateEmployee(101, "Jayvant", "Bad", DateTime.Today);
+                        DateTime date = DateTime.Today;
+                        var employeeId = Convert.ToInt32(_testContextInstance.DataRow["EmployeeId"].ToString());
+                        var employeeName = _testContextInstance.DataRow["EmployeeName"].ToString();
+                        var employeeRemark = _testContextInstance.DataRow["EmployeeRemark"].ToString();
+
+                        employeeList.CreateEmployee(employeeId, employeeName, employeeRemark, date);
                         string remark = "";
                         var retriveEmployee = employeeRetrive.SearchByRemark(remark);
                         Assert.AreEqual(retriveEmployee.RemarkText, remark);
@@ -223,6 +285,11 @@ namespace EmployeeManagementServiceFixture
         /// Below test case to check the employee Name.
         /// </summary>
         [TestMethod]
+        [DeploymentItem(@"EmployeeDataSource.xml")]
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.XML",
+                   @"EmployeeDataSource.xml",
+                   "Employee",
+                    DataAccessMethod.Sequential)]
         public void CheckEmployeeNameShouldThrow()
         {
             try
@@ -231,10 +298,15 @@ namespace EmployeeManagementServiceFixture
                 {
                     using (var employeeRetrive = new RetriveEmployeeServiceClient("WSHttpBinding_RetriveEmployeeService"))
                     {
-                        employeeList.CreateEmployee(101, "Jayvant", "Bad", DateTime.Today);
-                        string empName = "";
-                        var retriveEmployee = employeeRetrive.SearchByName(empName);
-                        Assert.AreEqual(retriveEmployee.EmployeeName, empName);
+                        DateTime date = DateTime.Today;
+                        var employeeId = Convert.ToInt32(_testContextInstance.DataRow["EmployeeId"].ToString());
+                        var employeeName = _testContextInstance.DataRow["EmployeeName"].ToString();
+                        var employeeRemark = _testContextInstance.DataRow["EmployeeRemark"].ToString();
+
+                        employeeList.CreateEmployee(employeeId, employeeName, employeeRemark, date);
+                        string employeeNameValue = "";
+                        var retriveEmployee = employeeRetrive.SearchByName(employeeNameValue);
+                        Assert.AreEqual(retriveEmployee.EmployeeName, employeeNameValue);
 
                     }
                 }
@@ -250,6 +322,12 @@ namespace EmployeeManagementServiceFixture
         /// Below test case to check the employee Id.
         /// </summary>
         [TestMethod]
+        [DeploymentItem(@"EmployeeDataSource.xml")]
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.XML",
+                   @"EmployeeDataSource.xml",
+                   "Employee",
+                    DataAccessMethod.Sequential)]
+
         public void CheckEmployeeIdShouldThrow()
         {
             try
@@ -258,10 +336,15 @@ namespace EmployeeManagementServiceFixture
                 {
                     using (var employeeRetrive = new RetriveEmployeeServiceClient("WSHttpBinding_RetriveEmployeeService"))
                     {
-                        employeeList.CreateEmployee(101, "Jayvant", "Bad", DateTime.Today);
-                        int empId = 0;
-                        var retriveEmployee = employeeRetrive.SearchById(empId);
-                        Assert.AreEqual(retriveEmployee.EmployeeId, empId);
+                        DateTime date = DateTime.Today;
+                        var employeeId = Convert.ToInt32(_testContextInstance.DataRow["EmployeeId"].ToString());
+                        var employeeName = _testContextInstance.DataRow["EmployeeName"].ToString();
+                        var employeeRemark = _testContextInstance.DataRow["EmployeeRemark"].ToString();
+
+                        employeeList.CreateEmployee(employeeId, employeeName, employeeRemark, date);
+                        int employeeIdValue = 0;
+                        var retriveEmployee = employeeRetrive.SearchById(employeeIdValue);
+                        Assert.AreEqual(retriveEmployee.EmployeeId, employeeIdValue);
 
                     }
                 }
@@ -278,14 +361,24 @@ namespace EmployeeManagementServiceFixture
         /// if employee id does not exist then should throw exception.
         /// </summary>
         [TestMethod]
+        [DeploymentItem(@"EmployeeDataSource.xml")]
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.XML",
+                   @"EmployeeDataSource.xml",
+                   "Employee",
+                    DataAccessMethod.Sequential)]
         public void DeleteEmployeeShouldThrow()
         {
             try
             {
                 using (var employeeList = new AddEmployeeServiceClient("BasicHttpBinding_AddEmployeeService"))
                 {
-                    employeeList.CreateEmployee(101, "Jayvant", "Bad", DateTime.Today);
-                    int employeeID = 0;
+                    DateTime date = DateTime.Today;
+                    var employeeId = Convert.ToInt32(_testContextInstance.DataRow["EmployeeId"].ToString());
+                    var employeeName = _testContextInstance.DataRow["EmployeeName"].ToString();
+                    var employeeRemark = _testContextInstance.DataRow["EmployeeRemark"].ToString();
+
+                    employeeList.CreateEmployee(employeeId, employeeName, employeeRemark, date);
+                    int employeeID = 101;
                     employeeList.DeleteEmployee(employeeID);
 
                 }
